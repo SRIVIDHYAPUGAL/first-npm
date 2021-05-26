65ae52cca47a90e6311f2b7dbdf9e99861326901
srividhya_pugazhenthi <srividhya.pugazhenthi@genesys.com>
2021-05-26 19:49:55 +0530

    days count

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,4 @@
 import moment = require('moment');
 export declare const Greeter: (name: string) => string;
 export declare const getDatePart: (m: moment.Moment) => moment.Moment;
+export declare const daysCount: (startDate: string, endDate: string, format?: string) => number;
