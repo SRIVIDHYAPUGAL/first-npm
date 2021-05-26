e81421d0a46e9410044ebc7ac36c4835ede87534
srividhya_pugazhenthi <srividhya.pugazhenthi@genesys.com>
2021-05-26 19:48:59 +0530

    days count

--- index.ts
+++ index.ts
@@ -4,3 +4,6 @@ export const Greeter = (name: string) => `Hello ${name}`;
 export const getDatePart= (m: moment.Moment) => {
     return moment([m.year(), m.month(), m.date()]);
 }
+export const daysCount = (startDate: string, endDate: string, format = 'YYYY-MM-DD'): number => {
+    return moment(endDate, format).diff(moment(startDate, format), 'days') + 1;
+}
