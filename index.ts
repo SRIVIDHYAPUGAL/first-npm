58887fdd20c083c6ecd6e685e82d12076f0a5119
srividhya_pugazhenthi <srividhya.pugazhenthi@genesys.com>
2021-05-26 18:57:54 +0530

    ready to publish

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
+import moment = require('moment');
 export const Greeter = (name: string) => `Hello ${name}`; 
+
+export const getDatePart= (m: moment.Moment) => {
+    return moment([m.year(), m.month(), m.date()]);
+}
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,3 @@
+import moment = require('moment');
 export declare const Greeter: (name: string) => string;
+export declare const getDatePart: (m: moment.Moment) => moment.Moment;
